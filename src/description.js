9e52afb0d9055c0f48653a8b5b620273d2f5e9fd
Nbregvadzzze <129889614+Nbregvadzzze@users.noreply.github.com>
2023-07-01 00:50:47 +0400

    upp

--- src/description.js
+++ src/description.js
@@ -1,32 +1,26 @@
-//ეს არის ერთი გვერდის ჩატვირთვისთვის
-
-// გავამზადეთ აპი ინფორაციები რაებიც გვჭირდბეოდა
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280/";
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
-// აქ წამოვიღეთ ყველა ელემენტი რომელიც ჰტმლ ში იჯდა რომელზეც დამუშავება გვინდოდა რომ გაგვეკეთებინა
+
 const main = document.getElementById("main");
 
 const similar = document.getElementById("similar");
 
-// აქ ლოკალურ მონაცეამთა ბაზიდან ამოვიღეთ ინორმაცია
+
 const movie = localStorage.getItem("movie");
 console.log(JSON.parse(movie));
-// შემოსული ინფორმაცია თავიდან არის სტრქინგი და მაგის გამო json.parse მეთოდით გავხადეთ ობიექტად
+
 const movieData = JSON.parse(movie);
 
-// ახალი დივი შევქმენით
+
 const movieDesc = document.createElement("div");
-// კლასი რომლეიც არის bootstrap კლასი
+
 movieDesc.classList.add("container");
 
-// ამ ობქიეტის ქონის გამო პირდპაირ შემიძლია movieDAta.ნებისმიერი პარამტერი რაცაა მაგის გამახება
-// და რადგანაც მასივი არაა არც foreach მჭირდება და არც map
-// აქედან დავხატეთ უქვე ერტი ფილმის აღწერა
 const clickBtn = () => {
-  const cart = JSON.parse(localStorage.getItem("cart")) || []; // Retrieve existing cart items or initialize an empty array
-  cart.push(movieData); // Add the new item to the cart array
-  localStorage.setItem("cart", JSON.stringify(cart)); // Store the updated cart back in local storage
+  const cart = JSON.parse(localStorage.getItem("cart")) || []; 
+  cart.push(movieData); 
+  localStorage.setItem("cart", JSON.stringify(cart)); 
 };
 
 movieDesc.innerHTML = `
@@ -45,68 +39,38 @@ movieDesc.innerHTML = `
       </div>
 `;
 
-// on btn click add item to localstorage
-
-// main დივში შვილებად ჩავ უგდეთ დახატული იფორმაცია
 main.appendChild(movieDesc);
 
-// დამატებით მოგვინდა რო 3 ელემენტი გამოჩნდეს რომლებიც არის მსგავსი ფილმები
 
-// // პარამეტრად გადავაწოდეთ აპი
 getMovies(API_URL);
-// ასიქნრონული ფუნქცია იმისთვის რომ აპის ფეჩინგი გავაკეთოთ
-// ასიქნრონულობას ვიყენებთ იმისთვის როცა სხვა მსიამართიდან ვიღებთ რამე ინფორამციას
-// დროი რო ჭირდება ჩასატვირთად მაგისთვის გვინდა რო დალოდება (await) გამოვიძახოთ
-// იქამდე დაელოდოს სანამ ყველაფერს არ ჩატვირთავს
 async function getMovies(url) {
   console.log(url);
 
-  // მისამართიდან მოაქვს ინფორამცია
+  
   const res = await fetch(url);
   console.log(res);
 
-  // ამას გადაყავს წამოღებული დატა ჯსონ ფორმატში
   const data = await res.json();
   console.log(data.results);
 
-  // აქ ვაწყვდით პარამეტრად ჯსონ ფორმატის ელემენტებს
-  // ეს მოდის როგორც
-  // [
-  //   {
-  //     adult: false,
-  //     backdrop_path: "/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg",
-  //     genre_ids: [28, 12, 53, 878],
-  //     id: 460465,
-  //   },
-  //    {
-  //     adult: false,
-  //     backdrop_path: "/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg",
-  //     genre_ids: [28, 12, 53, 878],
-  //     id: 460465,
-  //   },
-  // ]
   showMovies(data.results);
 }
 function showMovies(movies) {
   similar.innerHTML = " ";
 
-  // აქ ახალი ცვლდაით ჩვენ გამოვიყენეთ math.rand რომელიც მოცემული მასივიდან 0-20 მდე ინდექსებს
-  // შემთხვევითობის მეთოდით ალაგებს (012345 => 234612)
   const smallMovies = (movies = movies
     .sort(() => Math.random() - Math.random())
-    // აქ შმთხვევითობით გადაცვლილ ინდექსებიდან მხოლოდ 3 ელემენტს იღებს
+
     .slice(0, 3));
-  // დაგვჭირდა რო შეგვეცვალა foreEach ელემენტის მასივი და მიგვეწოდებინა ახალი smallMovies მასივი
   smallMovies.forEach((movie) => {
-    // წინასწარ რა ელემენტებიც იქნებოდა ობიექტში ჩასმული რაც მოდიოდა აპი დან
-    // წინასწარ შევქმენით ცვლადები რო აღარ დაგვეწერა movie.title
+    
     const { title, overview, original_language, vote_average, poster_path } =
       movie;
-    // ეს ქმნის div ელემენტს
+    
     const movieEl = document.createElement("div");
-    // ეს div ელემენტს უქმნის class col-4 ს
+  
     movieEl.classList.add("col-4");
-    // აქედან ვახდენთ დახატვას რო თითოეული ელემენტი როგორ გამოვიდეს
+
     movieEl.innerHTML = `
                 <div class="p-4">
                 <div class="movies">
@@ -125,20 +89,18 @@ function showMovies(movies) {
                 </div>
 
             `;
-    // ამით გამოძახებულ main დივს შიგნით შვილებად ვუმატებთ ყველა ელემენტს რომელიც ზემოით დავხატეთ
+  
     similar.appendChild(movieEl);
-    // დაჭერაზე ვქმნით ევენთს რომლეიც ბრაუზერის ლოკალურ მონაცემთა ბაზაში ამატებს ერთ ობიექტს
-    // რომეზეც დაჭრას ვახდენთ
+    
     movieEl.addEventListener("click", () => {
-      // აქედან ჩაემატა
+      
       localStorage.setItem("movie", JSON.stringify(movie));
-      // ახალ გვერდზე გადაგიყვანოს
+      
       window.location = "movie.html";
     });
   });
 }
 
-// ფუნქცია რომლეიც პარამეტრად იღებს vote_average რომ ფერების კონტროლი ქონდეს
 function getClassByVote(vote) {
   if (vote >= 7) {
     return "green";
@@ -148,8 +110,6 @@ function getClassByVote(vote) {
     return "red";
   }
 }
-
-// აქ უნდა შექმნათ ახალი ობიექტი თქვნეი ხლეით დაახლოებით უნდა იყოს
 
 const seats = [
   {
@@ -222,14 +182,13 @@ cartBtn.addEventListener("click", () => {
   console.log(cartBtn);
   const cart = JSON.parse(localStorage.getItem("cart"));
 
-  // from cart array get only vote_average and sum them
   const sum = cart.reduce((acc, item) => acc + item.vote_average, 0);
   console.log("sum", sum);
   const sumHTML = `<h1>ჯამი: ${sum}</h1>`;
   showSumItems.innerHTML = sumHTML;
   sumItems.appendChild(showSumItems);
 
-  let cartContent = ""; // Initialize an empty string to accumulate the HTML content
+  let cartContent = ""; 
 
   if (cart.length > 0) {
     cart.forEach((item, index) => {
@@ -244,25 +203,24 @@ cartBtn.addEventListener("click", () => {
             <button class="btn btn-primary remove-btn" data-index="${index}">Remove from cart</button>
           </div>
         </div>
-      `; // Modify this line to display the desired information for each item
+      `; 
     });
   } else {
     cartContent = `<h1>კალათა ცარიელია</h1>`;
   }
-  cartShowItems.innerHTML = cartContent; // Assign the accumulated HTML content to cartShowItems
+  cartShowItems.innerHTML = cartContent; 
 
   console.log("cartShowItems", cartShowItems);
   cartItems.appendChild(cartShowItems);
 
-  // Add event listeners to the "Remove from cart" buttons
   const removeButtons = document.querySelectorAll(".remove-btn");
   removeButtons.forEach((button) => {
     button.addEventListener("click", () => {
       const index = parseInt(button.getAttribute("data-index"));
-      cart.splice(index, 1); // Remove the item from the cart array
-      localStorage.setItem("cart", JSON.stringify(cart)); // Update the cart in localStorage
+      cart.splice(index, 1); 
+      localStorage.setItem("cart", JSON.stringify(cart)); 
 
-      // Refresh the cart display
+    
       cartItems.removeChild(cartShowItems);
       cartShowItems.innerHTML = "";
       cart.forEach((item, index) => {
@@ -281,7 +239,7 @@ cartBtn.addEventListener("click", () => {
       });
       cartItems.appendChild(cartShowItems);
 
-      // Recalculate the sum
+     
       const newSum = cart.reduce((acc, item) => acc + item.vote_average, 0);
       showSumItems.innerHTML = `<h1>ჯამი: ${newSum}</h1>`;
     });
--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-// გავამზადეთ აპი ინფორაციები რაებიც გვჭირდბეოდა
 
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
@@ -6,64 +5,36 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280/";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-// აქ წამოვიღეთ ყველა ელემენტი რომელიც ჰტმლ ში იჯდა რომელზეც დამუშავება გვინდოდა რომ გაგვეკეთებინა
 const main = document.querySelector("#main");
 const form = document.querySelector("#form");
 const search = document.querySelector("#search");
 
-// პარამეტრად გადავაწოდეთ აპი
 getMovies(API_URL);
 
-// ასიქნრონული ფუნქცია იმისთვის რომ აპის ფეჩინგი გავაკეთოთ
-// ასიქნრონულობას ვიყენებთ იმისთვის როცა სხვა მსიამართიდან ვიღებთ რამე ინფორამციას
-// დროი რო ჭირდება ჩასატვირთად მაგისთვის გვინდა რო დალოდება (await) გამოვიძახოთ
-// იქამდე დაელოდოს სანამ ყველაფერს არ ჩატვირთავს
 async function getMovies(url) {
   console.log(url);
 
-  // მისამართიდან მოაქვს ინფორამცია
   const res = await fetch(url);
   console.log(res);
 
-  // ამას გადაყავს წამოღებული დატა ჯსონ ფორმატში
   const data = await res.json();
   console.log(data.results);
 
-  // აქ ვაწყვდით პარამეტრად ჯსონ ფორმატის ელემენტებს
-  // ეს მოდის როგორც
-  // [
-  //   {
-  //     adult: false,
-  //     backdrop_path: "/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg",
-  //     genre_ids: [28, 12, 53, 878],
-  //     id: 460465,
-  //   },
-  //    {
-  //     adult: false,
-  //     backdrop_path: "/6MKr3KgOLmzOP6MSuZERO41Lpkt.jpg",
-  //     genre_ids: [28, 12, 53, 878],
-  //     id: 460465,
-  //   },
-  // ]
   showMovies(data.results);
 }
 
-// საიდანაც ვხატავთ ყველა ელემენტს
 function showMovies(movies) {
   main.innerHTML = " ";
 
-  // forEach არის მასივისთვის რო თითოეული ელემენტი წამოიღოს
   movies.forEach((movie) => {
-    // წინასწარ რა ელემენტებიც იქნებოდა ობიექტში ჩასმული რაც მოდიოდა აპი დან
-    // წინასწარ შევქმენით ცვლადები რო აღარ დაგვეწერა movie.title
+   
     const { title, overview, original_language, vote_average, poster_path } =
       movie;
-    // ეს ქმნის div ელემენტს
+
     const movieEl = document.createElement("div");
-    // ეს div ელემენტს უქმნის class col-4 ს
+ 
     movieEl.classList.add("col-4");
 
-    // აქედან ვახდენთ დახატვას რო თითოეული ელემენტი როგორ გამოვიდეს
     movieEl.innerHTML = `
                 <div class="p-4">
                 <div class="movies">
@@ -81,19 +52,15 @@ function showMovies(movies) {
                     </div>
                 </div>
             `;
-    // ამით გამოძახებულ main დივს შიგნით შვილებად ვუმატებთ ყველა ელემენტს რომელიც ზემოით დავხატეთ
+
     main.appendChild(movieEl);
-    // დაჭერაზე ვქმნით ევენთს რომლეიც ბრაუზერის ლოკალურ მონაცემთა ბაზაში ამატებს ერთ ობიექტს
-    // რომეზეც დაჭრას ვახდენთ
+
     movieEl.addEventListener("click", () => {
-      // აქედან ჩაემატა
       localStorage.setItem("movie", JSON.stringify(movie));
-      // ახალ გვერდზე გადაგიყვანოს
       window.location = "movie.html";
     });
   });
 }
-// ფუნქცია რომლეიც პარამეტრად იღებს vote_average რომ ფერების კონტროლი ქონდეს
 function getClassByVote(vote) {
   if (vote >= 7) {
     return "green";
@@ -103,21 +70,16 @@ function getClassByVote(vote) {
     return "red";
   }
 }
-// სერჩის ევენთია სუბმითზე რომ გააგზავნოს ახალი რექვესტი რო დახატაული ფილმები განაახლოს
+
 form.addEventListener("submit", (e) => {
-  // ფორმს როცა აქცვს თავისი submit ავტომრად არეფრეშებს გვერს და ეგ რო მოვაშოროტ მაგისთვის
   e.preventDefault();
-  // აქედან რაც ინფუთში ჩაიწერა მაგას ამოვიღებთ
   const searchTerm = search.value;
 
   console.log(searchTerm);
 
-  // თუ ჩაწერილი ტექსტი არსებობს ან არ არის ცარილი სტრინგი გამოძხოს რექვესტი
   if (searchTerm && searchTerm !== "") {
     getMovies(SEARCH_API + searchTerm);
-    // გამოძახების მერე წაშალოს ინფუთში შეყვანილი ტექსტი
     search.value = " ";
-    // თუ ზედა არ შესრულდა დაარეფრეშოს
   } else {
     window.location.reload;
   }
